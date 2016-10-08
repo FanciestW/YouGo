8c33c65852fa7b006cb5af6d7441cf4ab617fa5c
William Lin <wlin26@yahoo.com>
2016-10-08 12:12:27 -0400

    Unfinished work, reverting to single trip per user

--- Android/app/src/main/java/com/lintech/yougo/TripList.java
+++ Android/app/src/main/java/com/lintech/yougo/TripList.java
@@ -4,21 +4,31 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TripList extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference tripsDB = FirebaseDatabase.getInstance().getReference("trips");
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_list);
-        ListView list = (ListView)findViewById(R.id.tripListView);
-        list.
     }
 
     public void signout(View view){
